d7bc8843e82e1bf0916cc8963eedfcafd1accc86
kingfadzi <kingfadzi@gmail.com>
2025-02-03 20:50:43 -0500

    update violations.sql

--- sql/violations.sql
+++ sql/violations.sql
@@ -1,4 +1,7 @@
-DROP MATERIALIZED VIEW IF EXISTS combined_repo_violations;
+CREATE INDEX IF NOT EXISTS idx_kantra_violations_repo_id ON kantra_violations(repo_id);
+CREATE INDEX IF NOT EXISTS idx_kantra_violation_labels_violation_id ON kantra_violation_labels(violation_id);
+
+DROP MATERIALIZED VIEW IF EXISTS combined_repo_violations CASCADE;
 
 CREATE MATERIALIZED VIEW combined_repo_violations AS
 SELECT
@@ -21,10 +24,12 @@ SELECT
     l.key AS label_key,
     l.value AS label_value
 FROM combined_repo_metrics crm
-LEFT JOIN kantra_violations v
-       ON crm.repo_id = v.repo_id
-LEFT JOIN kantra_violation_labels vl
-       ON v.id = vl.violation_id
-LEFT JOIN kantra_labels l
-       ON vl.label_id = l.id
+LEFT JOIN kantra_violations v ON crm.repo_id = v.repo_id
+LEFT JOIN kantra_violation_labels vl ON v.id = vl.violation_id
+LEFT JOIN kantra_labels l ON vl.label_id = l.id
 ORDER BY crm.repo_id, v.id;
+
+CREATE INDEX IF NOT EXISTS idx_combined_repo_violations_label_key ON combined_repo_violations(label_key);
+CREATE INDEX IF NOT EXISTS idx_combined_repo_violations_label_composite ON combined_repo_violations(label_key, label_value, repo_id);
+
+REFRESH MATERIALIZED VIEW CONCURRENTLY combined_repo_violations;
