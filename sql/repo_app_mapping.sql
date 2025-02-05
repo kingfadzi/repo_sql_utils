d1054f9f19e7319eca5e28563f6e6536113c3945
Fadzi Ushewokunze <kingfadzi@gmail.com>
2025-02-05 09:48:18 -0500

    add all_languages

--- sql/repo_app_mapping.sql
+++ sql/repo_app_mapping.sql
@@ -1,3 +1,8 @@
+WITH distinct_business_apps AS (
+    SELECT DISTINCT component_id, identifier
+    FROM component_mapping
+    WHERE mapping_type = 'business_application'
+)
 SELECT
     (vc.project_key || '/' || vc.repo_slug) AS repo_id,
     vc.component_id,
@@ -6,13 +11,12 @@ SELECT
     vc.name AS version_control_name,
     vc.identifier AS version_control_identifier,
     vc.web_url,
-    string_agg(DISTINCT ba.identifier, ', ') AS app_identifiers
+    string_agg(dba.identifier, ', ') AS app_identifiers
 FROM
     component_mapping vc
         LEFT JOIN
-    component_mapping ba
-    ON vc.component_id = ba.component_id
-        AND ba.mapping_type = 'business_application'
+    distinct_business_apps dba
+    ON vc.component_id = dba.component_id
 WHERE
     vc.mapping_type = 'version_control'
 GROUP BY
@@ -25,4 +29,4 @@ GROUP BY
     vc.identifier,
     vc.web_url
 HAVING
-    COUNT(ba.identifier) > 0;
+    COUNT(dba.identifier) > 0;
