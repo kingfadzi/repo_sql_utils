42cc7f76bdca6369af349c520773cbb312a440ba
Fadzi Ushewokunze <kingfadzi@gmail.com>
2025-02-06 14:19:22 -0500

    add all_languages

--- utils/git_ssh_to_https.py
+++ utils/git_ssh_to_https.py
@@ -3,18 +3,16 @@ import csv
 def convert_ssh_to_https(ssh_url):
     prefix = "ssh://git@"
     suffix = ".git"
-
     if ssh_url.startswith(prefix) and ssh_url.endswith(suffix):
-        url_without_prefix = ssh_url[len(prefix):]
-        url_without_suffix = url_without_prefix[:-len(suffix)]
-
-        host_and_path = url_without_suffix.split(":", 1)
-        host = host_and_path[0]
-        path = host_and_path[1]
-
-        project, slug = path.split("/", 1)
-
-        return f"https://{host}/sc/{project}/{slug}.git"
+        url = ssh_url[len(prefix):-len(suffix)]
+        try:
+            host, remainder = url.split(":", 1)
+            _, path = remainder.split("/", 1)
+            project, slug = path.split("/", 1)
+            return f"https://{host}/scm/{project}/{slug}.git"
+        except Exception as e:
+            print(f"Error parsing URL '{ssh_url}': {e}")
+            return ssh_url
     else:
         print(f"Invalid URL format: {ssh_url}")
         return ssh_url
@@ -24,17 +22,14 @@ def convert_csv(input_file, output_file):
         reader = csv.DictReader(infile)
         fieldnames = reader.fieldnames + ['clone_url_https']
         writer = csv.DictWriter(outfile, fieldnames=fieldnames)
-
         writer.writeheader()
-
         for row in reader:
-            ssh_url = row['clone_url_ssh']
-            https_url = convert_ssh_to_https(ssh_url)
-            row['clone_url_https'] = https_url
+            ssh_url = row.get('clone_url_ssh', '')
+            row['clone_url_https'] = convert_ssh_to_https(ssh_url)
             writer.writerow(row)
 
 if __name__ == "__main__":
-    input_file = 'input.csv'  # Path to your input CSV file
-    output_file = 'output.csv'  # Path to your output CSV file
+    input_file = 'input.csv'
+    output_file = 'output.csv'
     convert_csv(input_file, output_file)
     print("Conversion completed!")
