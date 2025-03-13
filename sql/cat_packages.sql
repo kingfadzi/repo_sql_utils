893c2648cd72c38c64e343f37a5ea9d0eeb9d5aa
Fadzi Ushewokunze <kingfadzi@gmail.com>
2025-03-13 18:24:19 -0400

    cats spring

---
+++ sql/cat_packages.sql
@@ -0,0 +1,20 @@
+SELECT
+    SPLIT_PART(name, ':', 1) AS group_id,
+    SPLIT_PART(name, ':', 2) AS artifact_id,
+    COUNT(*) AS total_occurrences
+FROM categorized_dependencies_mv
+WHERE package_type IN ('maven', 'gradle')
+GROUP BY 1, 2
+ORDER BY total_occurrences DESC;
+
+---
+
+SELECT
+    SPLIT_PART(name, ':', 1) AS group_id,
+    COUNT(*) AS total_occurrences
+FROM categorized_dependencies_mv
+WHERE package_type IN ('maven', 'gradle')
+GROUP BY
+    SPLIT_PART(name, ':', 1)
+ORDER BY
+    total_occurrences DESC;
