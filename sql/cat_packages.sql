6763137fbcd66f2b3995dfc34dca97df1c51393e
Fadzi Ushewokunze <kingfadzi@gmail.com>
2025-03-14 14:06:49 -0400

    cats spring

--- sql/cat_packages.sql
+++ sql/cat_packages.sql
@@ -40,3 +40,15 @@ FROM (
          GROUP BY group_id
          ORDER BY total_repos DESC
      ) AS ordered_groups;
+
+--
+
+SELECT
+    SPLIT_PART(SPLIT_PART(name, ':', 1), '.', 1) AS part1,
+    SPLIT_PART(SPLIT_PART(name, ':', 1), '.', 2) AS part2,
+    SPLIT_PART(SPLIT_PART(name, ':', 1), '.', 3) AS part3,
+    COUNT(DISTINCT repo_id) AS total_repos
+FROM categorized_dependencies_mv
+WHERE package_type IN ('maven', 'gradle')
+GROUP BY part1, part2, part3
+ORDER BY total_repos DESC;
