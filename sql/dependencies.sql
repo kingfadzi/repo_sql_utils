8d918856ad63aa2f9d3c6df3f3716b8afe8bd6f9
Fadzi Ushewokunze <kingfadzi@gmail.com>
2025-03-16 09:43:40 -0400

    cats spring

--- sql/dependencies.sql
+++ sql/dependencies.sql
@@ -1,15 +1,21 @@
-SELECT cr.repo_id, cr.main_language, COUNT(d.repo_id) AS dependency_count
-FROM combined_repo_metrics cr
-         LEFT JOIN dependencies d ON cr.repo_id = d.repo_id
-GROUP BY cr.repo_id, cr.main_language;
-
-
 SELECT
     cr.repo_id,
     cr.main_language,
-    COALESCE(bt.tool, 'None') AS build_tool
+    cr.clone_url_ssh,
+    cr.status,
+    cr.web_url,
+    cr.classification_label,
+    cr.repo_size_bytes,
+    cr.activity_status,
+    COALESCE(bt.tool, 'None') AS build_tool,
+    COALESCE(dep_counts.dependency_count, 0) AS dependency_count
 FROM combined_repo_metrics cr
-         LEFT JOIN build_tools bt ON cr.repo_id = bt.repo_id;
+         LEFT JOIN build_tools bt ON cr.repo_id = bt.repo_id
+         LEFT JOIN (
+    SELECT repo_id, COUNT(*) AS dependency_count
+    FROM dependencies
+    GROUP BY repo_id
+) dep_counts ON cr.repo_id = dep_counts.repo_id;
 
 
 
