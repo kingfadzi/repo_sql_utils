4d17558ad79b640e23671b53de60dfdce1b972a2
kingfadzi <kingfadzi@gmail.com>
2025-04-11 11:40:10 -0400

    x

--- sql/urls.sql
+++ sql/urls.sql
@@ -6,24 +6,28 @@ regexp_replace(
     '\.git$', '', 'g'
 )
 
-UPDATE Bitbucket_Repositories
+UPDATE Your_Table
 SET browse_url =
-        regexp_replace(
-                regexp_replace(clone_url_ssh,
-                               '^git@(.*?):',
-                               'https://\1/',
-                               'g'
-                ),
-                '\.git$',
-                '',
-                'g'
-        )
+        CASE
+            -- GitLab Pattern (git@domain:path)
+            WHEN clone_url_ssh ~ '^git@([^/:]+):([^/]+/[^/]+)(\.git)?$' THEN
+                regexp_replace(
+                        clone_url_ssh,
+                        '^git@([^/:]+):([^/]+/[^/]+)(\.git)?$',
+                        'https://\1/\2',
+                        'g'
+                )
 
-UPDATE Bitbucket_Repositories
-SET browse_url =
-        regexp_replace(
-                clone_url_ssh,
-                '^(ssh://)?git@([^/:]+?)(:\d+)?/([^/]+)/([^/]+?)(\.git)?$',
-                'https://\2/scm/\4/\5',
-                'g'
-        )
+            -- BitBucket Pattern (ssh://git@domain:port/path)
+            WHEN clone_url_ssh ~ '^(ssh://)?git@([^/:]+?)(:\d+)?/([^/]+)/([^/]+?)(\.git)?$' THEN
+                regexp_replace(
+                        clone_url_ssh,
+                        '^(ssh://)?git@([^/:]+?)(:\d+)?/([^/]+)/([^/]+?)(\.git)?$',
+                        'https://\2/scm/\4/\5',
+                        'g'
+                )
+
+            -- Fallback: Leave unchanged
+            ELSE browse_url
+            END
+WHERE clone_url_ssh ~ 'git@';  -- Only process SSH URLs
