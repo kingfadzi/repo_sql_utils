880ced74f752b3c719702aae2129659c0ac874ea
kingfadzi <kingfadzi@gmail.com>
2025-04-11 10:58:28 -0400

    x

---
+++ sql/urls.sql
@@ -0,0 +1,29 @@
+UPDATE Bitbucket_Repositories
+SET browse_url =
+'https://' ||
+regexp_replace(
+    regexp_replace(clone_url_ssh, '^git@(.*?):', '\1/'),
+    '\.git$', '', 'g'
+)
+
+UPDATE Bitbucket_Repositories
+SET browse_url =
+        regexp_replace(
+                regexp_replace(clone_url_ssh,
+                               '^git@(.*?):',
+                               'https://\1/',
+                               'g'
+                ),
+                '\.git$',
+                '',
+                'g'
+        )
+
+UPDATE Bitbucket_Repositories
+SET browse_url =
+        regexp_replace(
+                clone_url_ssh,
+                '^(ssh://)?git@([^/:]+)(:\d+)?/([^/]+)/([^/]+)(\.git)?$',
+                'https://\2/scm/\4/repos/\5',
+                'g'
+        )
