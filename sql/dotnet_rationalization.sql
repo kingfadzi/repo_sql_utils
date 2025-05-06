d32ab7bc8429c4c8b9dda95ebc2731b9f897e074
kingfadzi <kingfadzi@gmail.com>
2025-05-06 11:53:37 -0400

    x

--- sql/dotnet_rationalization.sql
+++ sql/dotnet_rationalization.sql
@@ -1,11 +1,11 @@
 CASE
-    -- .NET 5+ unified platform (e.g. net60, net6.0, net6.0-windows)
-    WHEN runtime_version ~ '(^|;)\.?(net[5-9][0-9]{1,2}(\.\d+)?([-a-z0-9]*)?)' THEN
-        'NET ' || regexp_replace(runtime_version, '.*(^|;)\.?(net([5-9])0).*', '\3')
+    -- .NET 5+ (e.g. net5, net5.0, net50, net60, net6.0, net8.0-windows, net07.0)
+    WHEN runtime_version ~ '(^|;)net0?([5-9])0?(\.\d+)?([-a-z0-9]*)?' THEN
+        'NET ' || regexp_replace(runtime_version, '.*(^|;)net0?([5-9])0?.*', '\2')
 
-    -- .NET Core (e.g. netcoreapp3.1)
-    WHEN runtime_version ~ '(^|;)netcoreapp(\d+)' THEN
-        'NET Core ' || regexp_replace(runtime_version, '.*(^|;)netcoreapp(\d+).*', '\2')
+    -- .NET Core (e.g. netcoreapp3.1, netcoreapp2.0)
+    WHEN runtime_version ~ '(^|;)netcoreapp(\d+(\.\d+)?)' THEN
+        'NET Core ' || regexp_replace(runtime_version, '.*(^|;)netcoreapp(\d+(\.\d+)?).*', '\2')
 
     -- .NET Standard 2.1 → .NET Core 3.0+
     WHEN runtime_version ~ '(^|;)netstandard2\.1' THEN
@@ -19,7 +19,7 @@ CASE
     WHEN runtime_version ~ '(^|;)netstandard1\.' THEN
         'NET Core 1.x'
 
-    -- .NET Framework (e.g. net472)
+    -- .NET Framework (e.g. net472, net48)
     WHEN runtime_version ~ '(^|;)net4\d{1,2}' THEN
         'NET Framework'
 
