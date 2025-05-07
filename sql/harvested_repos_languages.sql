8dd35fc217b333e37be6f61e6eebd8d5ac0a2906
kingfadzi <kingfadzi@gmail.com>
2025-05-07 16:32:40 -0400

    x

--- sql/harvested_repos_languages.sql
+++ sql/harvested_repos_languages.sql
@@ -1,21 +1,11 @@
 SELECT
     hr.repo_id,
-    hr.host_name,
+    hr.browse_url,
     hr.main_language,
     hr.classification_label,
     hr.activity_status,
     hr.status,
-    gea.id AS go_enry_id,
-    gea.language AS go_enry_language,
-    gea.percent_usage AS go_enry_percent_usage,
-    bt.id AS build_tool_id,
-    bt.tool AS build_tool,
-    bt.tool_version,
-    bt.runtime_version,
-    bt.confidence AS build_tool_confidence,
-    bt.status AS build_tool_status,
-    bt.extraction_method
+    STRING_AGG(DISTINCT bt.tool || ':' || bt.tool_version, ',') AS build_tools_versions
 FROM public.harvested_repositories hr
-         LEFT JOIN public.go_enry_analysis gea ON hr.repo_id = gea.repo_id
          LEFT JOIN public.build_tools bt ON hr.repo_id = bt.repo_id
-    LIMIT 1048575;
+GROUP BY hr.repo_id, hr.browse_url, hr.main_language, hr.classification_label, hr.activity_status, hr.status
