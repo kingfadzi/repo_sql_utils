e38123c324cd213e059f7549647bef04c212e33b
kingfadzi <kingfadzi@gmail.com>
2025-05-07 16:39:31 -0400

    x

--- sql/harvested_repos_languages.sql
+++ sql/harvested_repos_languages.sql
@@ -8,11 +8,16 @@ SELECT
     STRING_AGG(DISTINCT bt.tool || ':' || bt.tool_version, ',') AS build_tools_versions
 FROM public.harvested_repositories hr
          LEFT JOIN public.build_tools bt ON hr.repo_id = bt.repo_id
+         INNER JOIN public.go_enry_analysis ge ON hr.repo_id = ge.repo_id
 WHERE hr.activity_status = 'ACTIVE'
+  AND ge.language IN (
+                      'Python', 'Java', 'JavaScript', 'Groovy', 'Go', 'ASP.NET', 'C#', 'F#',
+                      'Jinja', 'Kotlin', 'Jupyter Notebook', 'Visual Basic.NET', 'TypeScript', 'Visual Basic 6.0'
+    )
 GROUP BY
     hr.repo_id,
     hr.browse_url,
     hr.main_language,
     hr.classification_label,
     hr.activity_status,
-    hr.status
+    hr.status;
