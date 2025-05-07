07dc5da73a76b2632509150f5424d5e73f595db4
kingfadzi <kingfadzi@gmail.com>
2025-05-07 09:24:33 -0400

    x

--- sql/python_rationalization.sql
+++ sql/python_rationalization.sql
@@ -1,37 +1,25 @@
 CASE
-    -- Clean spaces around delimiters
-    WHEN regexp_replace(runtime_version, '\s*(,|\|)\s*', '\1', 'g') ~ '>=\d+\.\d+' THEN
-        regexp_replace(
-            regexp_replace(runtime_version, '\s*(,|\|)\s*', '\1', 'g'),
-            '.*>=\s*(\d+\.\d+).*',
-            '\1'
-        )
+  WHEN runtime_version LIKE '%|%' THEN
+    regexp_replace(
+      regexp_replace(
+        split_part(runtime_version, '|', 1),
+        '[\^<>=~! ]+',
+        '',
+        'g'
+      ),
+      '^(\d+\.\d+)(?:\.\d+)?$',
+      '\1'
+    )
 
-    WHEN regexp_replace(runtime_version, '\s*(,|\|)\s*', '\1', 'g') ~ '\d+\.\d+\.\*' THEN
-        regexp_replace(
-            regexp_replace(runtime_version, '\s*(,|\|)\s*', '\1', 'g'),
-            '.*?(\d+\.\d+)\.\*.*',
-            '\1'
-        )
-
-    WHEN regexp_replace(runtime_version, '\s*(,|\|)\s*', '\1', 'g') ~ '\^?\d+\.\d+(?:\|\^?\d+\.\d+)*' THEN
-        regexp_replace(
-            regexp_replace(runtime_version, '\s*(,|\|)\s*', '\1', 'g'),
-            '.*?\^?(\d+\.\d+).*',
-            '\1'
-        )
-
-    WHEN runtime_version ~ '>=\s*\d+\.\d+(\.\d+)?' THEN
-        regexp_replace(runtime_version, '.*>=\s*(\d+\.\d+)(?:\.\d+)?[^0-9]?.*', '\1')
-
-    WHEN runtime_version ~ '\d+\.\d+\.\d+' THEN
-        regexp_replace(runtime_version, '.*?(\d+\.\d+)\.\d+.*', '\1')
-
-    WHEN runtime_version ~ '\d+\.\d+' THEN
-        regexp_replace(runtime_version, '.*?(\d+\.\d+).*', '\1')
-
-    WHEN runtime_version ~ '\m\d+\M' THEN
-        regexp_replace(runtime_version, '.*?\m(\d+)\M.*', '\1.0')
-
-    ELSE runtime_version
+  ELSE
+    regexp_replace(
+      regexp_replace(
+        split_part(runtime_version, ',', 1),
+        '[\^<>=~! ]+',
+        '',
+        'g'
+      ),
+      '^(\d+\.\d+)(?:\.\d+)?$',
+      '\1'
+    )
 END
