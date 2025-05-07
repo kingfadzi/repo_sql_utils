d444fb72abd4f5ba3880782ceacfc7e796b40cc3
kingfadzi <kingfadzi@gmail.com>
2025-05-07 10:00:50 -0400

    x

--- sql/harvested_repos_languages.sql
+++ sql/harvested_repos_languages.sql
@@ -1,23 +1,10 @@
 SELECT
     hr.repo_id,
-    hr.repo_name,
-    hr.project_key,
-    hr.repo_slug,
-    hr.clone_url_ssh,
-    hr.browse_url,
-    hr.updated_date,
-    hr.last_harvested_at,
-    hr.app_id,
     hr.host_name,
-    hr.transaction_cycle,
     hr.main_language,
     hr.classification_label,
     hr.activity_status,
     hr.status,
-    hr.comment,
-    hr.scope,
-    hr.component_id,
-    hr.component_name,
     gea.id AS go_enry_id,
     gea.language AS go_enry_language,
     gea.percent_usage AS go_enry_percent_usage,
@@ -31,4 +18,4 @@ SELECT
 FROM public.harvested_repositories hr
          LEFT JOIN public.go_enry_analysis gea ON hr.repo_id = gea.repo_id
          LEFT JOIN public.build_tools bt ON hr.repo_id = bt.repo_id
-
+    LIMIT 1048575;
