9ed66961626bf9d442dd62b57bdecd3e63a2a3d9
kingfadzi <kingfadzi@gmail.com>
2025-05-07 16:35:30 -0400

    x

--- sql/harvested_repos_languages.sql
+++ sql/harvested_repos_languages.sql
@@ -8,4 +8,11 @@ SELECT
     STRING_AGG(DISTINCT bt.tool || ':' || bt.tool_version, ',') AS build_tools_versions
 FROM public.harvested_repositories hr
          LEFT JOIN public.build_tools bt ON hr.repo_id = bt.repo_id
-GROUP BY hr.repo_id, hr.browse_url, hr.main_language, hr.classification_label, hr.activity_status, hr.status
+WHERE hr.activity_status = 'ACTIVE'
+GROUP BY
+    hr.repo_id,
+    hr.browse_url,
+    hr.main_language,
+    hr.classification_label,
+    hr.activity_status,
+    hr.status
