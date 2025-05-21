9ab7b4c0b259fdb3d2b3cde97749b075fcd45875
kingfadzi <kingfadzi@gmail.com>
2025-05-21 19:53:44 -0400

    x

--- sql/build_cache_uc.sql
+++ sql/build_cache_uc.sql
@@ -1,8 +1,8 @@
 -- First, drop the existing unique constraint
 ALTER TABLE build_config_cache
-DROP CONSTRAINT IF EXISTS _build_tools_full_uc;
+DROP CONSTRAINT IF EXISTS _build_config_cache_full_uc;
 
 -- Then, create the new unique constraint including `variant`
 ALTER TABLE build_config_cache
-    ADD CONSTRAINT _build_tools_full_uc
+    ADD CONSTRAINT _build_config_cache_full_uc
         UNIQUE (repo_id, module_path, tool, variant, tool_version, runtime_version)
