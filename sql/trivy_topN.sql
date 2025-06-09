d6f3bfb117f03517cabd715f2dc0c21b203d233e
kingfadzi <kingfadzi@gmail.com>
2025-06-09 14:07:18 -0400

    x

--- sql/trivy_topN.sql
+++ sql/trivy_topN.sql
@@ -1,19 +1,23 @@
+WITH product_info AS (
+    SELECT
+        repo_id,
+        CASE
+            WHEN resource_type = 'pom' THEN SPLIT_PART(pkg_name, ':', 1)
+            ELSE pkg_name
+            END AS product_name,
+        resource_type,
+        severity
+    FROM trivy_vulnerability
+    WHERE pkg_name IS NOT NULL
+)
 SELECT
-    CASE
-        WHEN resource_type = 'pom' THEN SPLIT_PART(pkg_name, ':', 1)
-        ELSE pkg_name
-        END AS product_name,
+    product_name,
     resource_type,
     severity,
     COUNT(DISTINCT repo_id) AS repo_count
-FROM trivy_vulnerability
-WHERE pkg_name IS NOT NULL
-GROUP BY
-    CASE
-        WHEN resource_type = 'pom' THEN SPLIT_PART(pkg_name, ':', 1)
-        ELSE pkg_name
-        END,
-    resource_type,
-    severity
+FROM product_info
+         JOIN harvested_repositories USING (repo_id)
+
+GROUP BY product_name, resource_type, severity
 ORDER BY repo_count DESC
 
