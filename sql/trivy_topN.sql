23f704793815aded2d3156fcc2dba3443d51dfbe
kingfadzi <kingfadzi@gmail.com>
2025-06-09 14:25:03 -0400

    x

--- sql/trivy_topN.sql
+++ sql/trivy_topN.sql
@@ -1,23 +1,27 @@
-WITH product_info AS (
-    SELECT
-        repo_id,
-        CASE
-            WHEN resource_type = 'pom' THEN SPLIT_PART(pkg_name, ':', 1)
-            ELSE pkg_name
-            END AS product_name,
-        resource_type,
-        severity
-    FROM trivy_vulnerability
-    WHERE pkg_name IS NOT NULL
-)
 SELECT
-    product_name,
+    CASE WHEN resource_type = 'pom'
+             THEN SPLIT_PART(pkg_name, ':', 1)
+         ELSE pkg_name
+        END AS product,
+    resource_class,
     resource_type,
-    severity,
-    COUNT(DISTINCT repo_id) AS repo_count
-FROM product_info
-         JOIN harvested_repositories USING (repo_id)
-
-GROUP BY product_name, resource_type, severity
-ORDER BY repo_count DESC
-
+    COUNT(*) AS total_vulnerabilities,
+    COUNT(DISTINCT repo_id) AS affected_repositories,
+    COUNT(*) FILTER (WHERE severity = 'CRITICAL') AS critical_count,
+        COUNT(DISTINCT repo_id) FILTER (WHERE severity = 'CRITICAL') AS critical_repos,
+        COUNT(*) FILTER (WHERE severity = 'HIGH') AS high_count,
+        COUNT(DISTINCT repo_id) FILTER (WHERE severity = 'HIGH') AS high_repos,
+        COUNT(*) FILTER (WHERE severity = 'MEDIUM') AS medium_count,
+        COUNT(DISTINCT repo_id) FILTER (WHERE severity = 'MEDIUM') AS medium_repos,
+        COUNT(*) FILTER (WHERE severity = 'LOW') AS low_count,
+        COUNT(DISTINCT repo_id) FILTER (WHERE severity = 'LOW') AS low_repos
+FROM trivy_vulnerability
+GROUP BY
+    CASE WHEN resource_type = 'pom'
+             THEN SPLIT_PART(pkg_name, ':', 1)
+         ELSE pkg_name
+        END,
+    resource_class,
+    resource_type
+ORDER BY total_vulnerabilities DESC
+    LIMIT 10;
