b7167151f1af5233a75136182f05653e125979ab
kingfadzi <kingfadzi@gmail.com>
2025-06-15 16:12:14 -0400

    x

--- sql/kipis.sql
+++ sql/kipis.sql
@@ -31,3 +31,19 @@ CREATE INDEX ON combined_repo_metrics_api (app_id);
 CREATE INDEX ON combined_repo_metrics_api (avg_cyclomatic_complexity);
 CREATE INDEX ON combined_repo_metrics_api (repo_size_bytes);
 CREATE INDEX ON combined_repo_metrics_api (all_languages);
+
+----
+
+SELECT
+    hr.main_language,
+    l.type
+FROM harvested_repositories hr
+         LEFT JOIN languages l ON LOWER(hr.main_language) = LOWER(l.name)
+WHERE NOT (
+    l.type = 'programming'
+        OR LOWER(hr.main_language) = 'no language'
+        OR LOWER(l.type) IN ('markup', 'data')
+    )
+  AND hr.main_language IS NOT NULL
+GROUP BY hr.main_language, l.type
+ORDER BY COUNT(*) DESC;
